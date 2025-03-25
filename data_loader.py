44108751e2402440a11fe8905ca0db451f6c56ea
ViniciusTheCoder <vinigurskiferraz@gmail.com>
2025-03-25 16:00:00 -0300

    docs: removing useless docs

--- data_loader.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-def load_dataset(path: str) -> pd.DataFrame:
-    df = pd.read_csv(path)
-
-    return df
-
-dataframe = load_dataset("data/product_order_details.csv")
--- models.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class QuestionRequest(BaseModel):
-    question: str
-
-class AnswerResponse(BaseModel):
-    answer: str
