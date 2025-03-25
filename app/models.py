22c0cee01dfb4393d9ae842b0062fb860e96e78b
ViniciusTheCoder <vinigurskiferraz@gmail.com>
2025-03-25 19:08:29 -0300

    feat: creating type checkers with pydantic

--- app/models.py
+++ app/models.py
@@ -1,7 +1,18 @@
 from pydantic import BaseModel
+from typing import Any, List, Dict, Union
 
 class QuestionRequest(BaseModel):
     question: str
+
+class GeneratedQuestion(BaseModel):
+    question: str
+    answer: str
+
+class FinalResponse(BaseModel):
+    question: str
+    
+    answer: Union[str, float, List[Any], Dict[str, Any]]
+    generated_questions: List[GeneratedQuestion]
 
 class AnswerResponse(BaseModel):
     answer: str
